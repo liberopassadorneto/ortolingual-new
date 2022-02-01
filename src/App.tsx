ce1f543d9b6410ce9b94f3be54c0b1814a9e55f6
libero <lneto@tecnologia-unica.com.br>
2022-02-01 19:58:38 -0400

    adming ReactPixel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import ReactPixel from 'react-facebook-pixel';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styles from './App.module.scss';
 import { Home } from './pages';
 import { Cwb } from './pages/cwb';
 import { ThankYouPage } from './pages/thankyou';
 
+const options = {
+  autoConfig: true,
+  debug: false,
+};
+
 export function App() {
+  ReactPixel.init('935971993835238', undefined, options);
+  ReactPixel.pageView();
   return (
     <Router>
       {/* <Navbar /> */}
--- src/pages/thankyou.tsx
+++ src/pages/thankyou.tsx
@@ -1,7 +1,10 @@
+import ReactPixel from 'react-facebook-pixel';
 import { ScrollToTop } from '../components/ScrollToTop';
 import { ThankYou } from '../components/ThankYou';
 
 export function ThankYouPage() {
+  ReactPixel.track('Lead', '');
+
   return (
     <>
       <ScrollToTop />
